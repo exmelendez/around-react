980c8c77b47dbe932e020d7628daff8580b7ce9d
EDDIE MELENDEZ <eddie@EDDIEs-Mac-mini.fios-router.home>
2021-03-28 22:55:54 -0400

    feat: added Api + const files from previous utisl & imported api user data/setup

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,16 +1,33 @@
+import {useState, useEffect} from  'react';
+import api from '../utils/Api';
+
+
 function Main(props) {
+  const [userName, setUserName] = useState("Cousteau");
+  const [userDescription, setUserDescription] = useState("Explorer");
+  const [userAvatar, setUserAvatar] = useState('images/avatar_image-min.jpg');
+
+  useEffect(() => {
+    api.getUserInfo()
+      .then(user => {
+        setUserName(user.name);
+        setUserDescription(user.about);
+        setUserAvatar(user.avatar);
+      })
+      .catch((err) => console.log(err));
+  }, []);
 
   return (
     <main className="content">
       <section className="profile">
-        <div className="profile__avatar">
+        <div className="profile__avatar" style={{ backgroundImage: `url(${userAvatar})` }}>
           <button className="profile__avatar-overlay" onClick={props.onEditAvatar}></button>
         </div>
 
         <div className="profile__info">
-          <p className="profile__name">Cousteau</p>
+          <p className="profile__name">{userName}</p>
           <button className="profile__edit-btn" onClick={props.onEditProfile} aria-label="Profile edit"></button>
-          <p className="profile__title">Explorer</p>
+          <p className="profile__title">{userDescription}</p>
         </div>
 
         <button className="profile__add-btn" onClick={props.onAddPlace} aria-label="Add card"></button>
---
+++ src/utils/Api.js
@@ -0,0 +1,90 @@
+class Api {
+  constructor({ baseUrl, headers }) {
+    this._baseUrl = baseUrl;
+    this._headers = headers;
+  }
+
+  addCard({ link, name }) {
+    return fetch(`${this._baseUrl}/cards`, {
+      headers: this._headers,
+      method: "POST",
+      body: JSON.stringify({
+        link,
+        name
+      })
+    })
+      .then((res) => res.ok ? res.json() : Promise.reject(`Error! ${res.statusText}`));
+  }
+
+  changeLikeCardStatus(cardId, isLiked) {
+    if (isLiked) {
+      return fetch(`${this._baseUrl}/cards/likes/${cardId}`, {
+        headers: this._headers,
+        method: "DELETE"
+      })
+        .then((res) => res.ok ? res.json() : Promise.reject(`Error! ${res.statusText}`));
+
+    } else {
+      return fetch(`${this._baseUrl}/cards/likes/${cardId}`, {
+        headers: this._headers,
+        method: "PUT"
+      })
+        .then((res) => res.ok ? res.json() : Promise.reject(`Error! ${res.statusText}`));
+    }
+  }
+
+  getCardList() {
+    return fetch(`${this._baseUrl}/cards`, {
+      headers: this._headers
+    })
+      .then((res) => res.ok ? res.json() : Promise.reject(`Error! ${res.statusText}`));
+  }
+
+  getUserInfo() {
+    return fetch(`${this._baseUrl}/users/me`, {
+      headers: this._headers
+    })
+      .then((res) => res.ok ? res.json() : Promise.reject(`Error! ${res.statusText}`));
+  }
+
+  removeCard(cardId) {
+    return fetch(`${this._baseUrl}/cards/${cardId}`, {
+      headers: this._headers,
+      method: "DELETE"
+    })
+      .then((res) => res.ok ? res.json() : Promise.reject(`Error! ${res.statusText}`));
+  }
+
+  setUserAvatar({ avatar }) {
+    return fetch(`${this._baseUrl}/users/me/avatar`, {
+      headers: this._headers,
+      method: "PATCH",
+      body: JSON.stringify({
+        avatar
+      })
+    })
+      .then((res) => res.ok ? res.json() : Promise.reject(`Error! ${res.statusText}`));
+  }
+
+  setUserInfo({ name, about }) {
+    return fetch(`${this._baseUrl}/users/me`, {
+      headers: this._headers,
+      method: "PATCH",
+      body: JSON.stringify({
+        name,
+        about
+      })
+    })
+      .then((res) => res.ok ? res.json() : Promise.reject(`Error! ${res.statusText}`));
+  }
+}
+
+const api = new Api({
+  baseUrl: "https://around.nomoreparties.co/v1/group-4",
+  headers: {
+    authorization: "9d5a3bcf-ed42-48db-a52f-3430aec59f7e",
+    "Content-Type": "application/json"
+  }
+});
+
+export default api;
