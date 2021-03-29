da26e1a4ab71dc39bf878984fe28a6e787c228ce
EDDIE MELENDEZ <eddie@EDDIEs-Mac-mini.fios-router.home>
2021-03-29 18:30:03 -0400

    bug: changed x/+ symbol to unicode in PopupWithForm component to display as a symbol

--- src/components/PopupWithForm.js
+++ src/components/PopupWithForm.js
@@ -1,9 +1,11 @@
 function PopupWithForm(props) {
+  const xSymbol = '\u002B';
+
   return (
     
     <div className={`modal modal_type_${props.name} ${props.isOpen ? 'modal_is-open' : ''}`} onClick={props.onClose}>
       <div className="modal__container">
-        <button className={`modal__close-btn`} aria-label="Close popup" onClick={props.onClose}>&plus;</button>
+        <button className={`modal__close-btn`} aria-label="Close popup" onClick={props.onClose}>{xSymbol}</button>
         <h2 className="modal__title">{props.title}</h2>
 
         <form className={`form form_type_${props.name}`}>
