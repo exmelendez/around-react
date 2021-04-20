69ed29ff02ac17ee89403c1e6784db3c1262e274
exmelendez <exmelendez@gmail.com>
2021-04-20 14:17:53 -0400

    mod EditProfilePopup component to use refs with input/onChange

--- src/components/EditProfilePopup.js
+++ src/components/EditProfilePopup.js
@@ -1,4 +1,4 @@
-import { useState, useContext, useEffect } from 'react';
+import { useState, useContext, useEffect, useRef } from 'react';
 import { CurrentUserContext } from '../contexts/CurrentUserContext';
 import PopupWithForm from './PopupWithForm';
 
@@ -7,13 +7,15 @@ function EditProfilePopup({onUpdateUser, isOpen, onClose}) {
 
   const [name, setName] = useState(currentUser.name);
   const [description, setDescription] = useState(currentUser.about);
+  const nameInputRef = useRef(name);
+  const descriptionInputRef = useRef(description);
 
-  function handleNameChange(e) {
-    setName(e.target.value);
+  function handleNameChange() {
+    setName(nameInputRef.current.value);
   }
 
-  function handleDescriptionChange(e) {
-    setDescription(e.target.value);
+  function handleDescriptionChange() {
+    setDescription(descriptionInputRef.current.value);
   }
 
   function handleSubmit(e) {
@@ -35,11 +37,11 @@ function EditProfilePopup({onUpdateUser, isOpen, onClose}) {
     <>
       <PopupWithForm name={"edit-profile"} title={"Edit profile"} isOpen={isOpen} onClose={onClose} handleSubmit={handleSubmit}>
         <input id="profile-name" onChange={handleNameChange} className="form__input form__input-profile-name" type="text" name="name" placeholder="Name"
-              minLength="2" maxLength="40" defaultValue={currentUser.name} required />
+              minLength="2" maxLength="40" ref={nameInputRef} defaultValue={currentUser.name} required />
         <span id="profile-name-error" className="form__error"></span>
 
         <input id="profile-title" onChange={handleDescriptionChange} className="form__input form__input-profile-title" type="text" name="about"
-          placeholder="Title" minLength="2" maxLength="200" defaultValue={currentUser.about} required />
+          placeholder="Title" minLength="2" maxLength="200" ref={descriptionInputRef} defaultValue={currentUser.about} required />
         <span id="profile-title-error" className="form__error"></span>
       </PopupWithForm>
     </>
